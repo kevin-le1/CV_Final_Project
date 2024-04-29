bb828d948715fe9945df2f4e821781fbe3551904
Kevin Le <kevin@dynamic-oit-ip4-wifirestricted03-10-48-155-202.princeton.edu>
2024-04-29 15:39:16 -0400

    oil spill images

---
+++ cnn/oil_images.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import urllib.request
+
+# Set the search query
+query = "oil_spills_in_water"
+
+# Set the number of images to download
+num_images = 100
+
+# Set the directory to save the images
+save_dir = query
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
+# Set the Google Images search URL
+url = f"https://www.google.com/search?q={query}&tbm=isch"
+
+# Send a request to the Google Images search page
+response = requests.get(url)
+
+# Parse the HTML content using BeautifulSoup
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Find all the image links
+image_links = [img.get("src") for img in soup.find_all("img")]
+
+# Download the images
+downloaded_images = 0
+for i, link in enumerate(image_links):
+    try:
+        urllib.request.urlretrieve(link, os.path.join(save_dir, f"{i}.jpg"))
+        print(f"Downloaded image {i+1}/{num_images}")
+        downloaded_images += 1
+        if downloaded_images >= num_images:
+            break
+    except:
+        print(f"Error downloading image {i+1}/{num_images}")
+
+print(f"Scraping complete! Downloaded {downloaded_images} images.")
